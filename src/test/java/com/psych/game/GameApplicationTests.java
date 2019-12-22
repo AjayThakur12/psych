1f9d264ddd8a45dfdd4e76f77558134061d605d4
AjayThakur12 <ajju4866@gmail.com>
2019-12-22 16:51:16 +0530

    init

---
+++ src/test/java/com/psych/game/GameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psych.game;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
